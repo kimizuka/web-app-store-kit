13491c3c9b8d8c89ba426fa8c7e4ef0fd838ff65
kimizuka <f.kimizuka@gmail.com>
2023-04-11 11:20:23 +0900

    add isInit

--- src/components/templates/IndexPageTemplate.tsx
+++ src/components/templates/IndexPageTemplate.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import { useResize } from '@/hooks/useResize';
 import { useIsStandalone } from '@/hooks/useIsStandalone';
 import { StoreArticle } from '@/components/organisms/StoreArticle';
@@ -7,37 +8,53 @@ import { AppArticle } from '@/components/organisms/AppArticle';
 
 const Wrapper = styled.div`
   line-height: 1em;
+
+  [data-is-init] {
+    opacity: 0;
+    transition: opacity .2s ease-in-out;
+
+    &[data-is-init='true'] {
+      opacity: 1;
+    }
+  }
 `;
 
 export function IndexPageTemplate() {
+  const [ isInit, setIsInit ] = useState(false);
   const { windowWidth, windowHeight } = useResize();
   const { isStandalone } = useIsStandalone({
     width: windowWidth,
     height: windowHeight
   });
 
+  useEffect(() => {
+    setIsInit(true);
+  }, [isStandalone]);
+
   return (
     <Wrapper>
       <Head>
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      {!isStandalone ? (
-        <StoreArticle
-          title="アプリタイトル"
-          author="作者名"
-          icon="https://via.placeholder.com/140x140"
-          screenshots={[
-            'https://via.placeholder.com/360x650',
-            'https://via.placeholder.com/360x650',
-            'https://via.placeholder.com/360x650',
-            'https://via.placeholder.com/360x650',
-            'https://via.placeholder.com/360x650'
-          ]}
-          description="ディスクリプション"
-        />
-      ) : (
-        <AppArticle />
-      )}
+      <div data-is-init={ isInit }>
+        {!isStandalone ? (
+          <StoreArticle
+            title="アプリタイトル"
+            author="作者名"
+            icon="https://via.placeholder.com/140x140"
+            screenshots={[
+              'https://via.placeholder.com/360x650',
+              'https://via.placeholder.com/360x650',
+              'https://via.placeholder.com/360x650',
+              'https://via.placeholder.com/360x650',
+              'https://via.placeholder.com/360x650'
+            ]}
+            description="ディスクリプション"
+          />
+        ) : (
+          <AppArticle />
+        )}
+      </div>
     </Wrapper>
   );
 }
